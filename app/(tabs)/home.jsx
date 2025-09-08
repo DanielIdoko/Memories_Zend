3f3c471f064694650a96bc2645d9d5d7241549ce
Daniel Idoko <danielidoko46@gmail.com>
2025-09-08 01:35:24 +0100

    Modify home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,59 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import homeStyles from "../../designs/home.designs";
+import { Feather } from "@expo/vector-icons";
+import CollectionCard from "../../components/CollectionCard";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <View>
-        
-      </View>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView style={homeStyles.main}>
+      {/* Header start */}
+      <ScrollView>
+        <View style={homeStyles.header}>
+          <Text
+            style={{
+              color: "#4B4C4C",
+              flex: 1,
+              fontSize: 14,
+              fontWeight: "bold",
+            }}
+          >
+            Hi, Welcome!
+          </Text>
+          <Feather
+            name="menu"
+            size={20}
+            style={{ color: "#121212" }}
+            onPress={() => {}}
+          />
+        </View>
+        {/* Header ends here */}
 
-export default Home
+        {/* Main content */}
+        <View 
+        style={{
+          padding: 10
+        }}>
+          <Text style={{
+            fontSize: 14
+          }}>Recents</Text>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            <CollectionCard />
+            <CollectionCard />
+            <CollectionCard />
+            <CollectionCard />
+            <CollectionCard />
+            <CollectionCard />
+            <CollectionCard />
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
--- designs/home.designs.js
+++ designs/home.designs.js
@@ -1,12 +1,22 @@
 import { StyleSheet } from "react-native";
 
 const homeStyles = StyleSheet.create({
+  main: {
+    backgroundColor: "#f1f1f1"
+  },
   header: {
     height: 44,
-    padding: 2,
+    padding: 10,
+    flexDirection: "row",
     alignItems: "center",
+    gap: 2
   },
-  
+  asideBar: {
+    width: 220,
+    borderRightWidth: 1,
+    borderRightColor: "#CBCBCB",
+    padding: 10,
+  },
 });
 
 export default homeStyles;
